0d28bf182597155688b7fe67bbc58785e23a5275
Jacco <jaccov.wijk@yahoo.com>
2021-10-05 15:02:15 +0200

    Update

---
+++ plot.py
@@ -0,0 +1,84 @@
+# from matplotlib.lines import _LineStyle
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+import regex as re
+
+# from numpy.core.fromnumeric import mean, std
+sys.path.insert(0, 'evoman') 
+
+def plot_fitness(general_names, N_runs, gens=20):
+    """
+    Plotting the fitness for all experiment names starting with general_names 
+    (taking the sigma also), getting the saved mean fitnesses and plotting the 
+    results. N_runs and gens are needed to specify respectively the amount of 
+    winners and the size of the fitness array saved in the numpy file.
+    """
+
+    # local_dir = os.path.dirname(__file__)
+    
+    # saving the enemy list and experiment names for specified general name
+    directories = [name for name in os.listdir(".") if os.path.isdir(name)]
+    enemies = []
+    experiment_names = []
+
+    for dir in directories:
+        for general_name in general_names:
+            if re.match(general_name, dir):
+                enemies.append(int(re.findall(r"enemy\d{1,2}", dir)[0][5:]))
+                experiment_names.append(dir)
+
+    # one big array to save the loaded fitness numpy files, shape: (exp.names, runs, mean/max, generations)
+    fitnesses = np.zeros((len(experiment_names), N_runs, 2, gens))
+
+    # loading the numpy files and saving in right indices    
+    for exp_id, experiment_name in enumerate(experiment_names):       
+        # plt.figure()
+        # plt.title(f"{experiment_name}")
+        for i in range(N_runs):
+            f_mean = np.load(f"{experiment_name}/fitness_gens_{i}.npy")
+            f_max = np.load(f"{experiment_name}/fitness_max_{i}.npy")
+            fitnesses[exp_id, i, :, :len(f_mean)] = np.array((f_mean, f_max))
+            # lines = []
+            # lines.append(plt.plot(f_mean, '-')[0])
+            # lines.append(plt.plot(f_max, '--')[0])
+
+    # plt.xlabel('generations')
+    # plt.ylabel('fitness')
+    # plt.legend(lines, ['mean', 'max'])
+    
+    # create a figure for every enemy and plot the non sigma and sigma fitnesses
+    for i, experiment_name in enumerate(experiment_names):
+        plt.figure(enemies[i])
+        
+        fitnesses[fitnesses==0] = np.nan
+        mean_mean_fitness = np.nanmean(fitnesses[i,:,0,:], axis=0)
+        stdev_mean_fitness = np.nanstd(fitnesses[i,:,0,:], axis=0)
+        mean_mean_fitness = mean_mean_fitness[mean_mean_fitness != 0]
+        
+        # second half of indices are sigma scaled
+        if (i < len(experiment_names)/2):
+            color = 'red'
+            label = 'default fitness'
+        else:
+            color = 'blue'
+            label = 'sigma scaled fitness'
+        plt.plot(mean_mean_fitness, '-', label=label, color=color)
+        plt.fill_between(np.arange(0, len(mean_mean_fitness)), mean_mean_fitness - stdev_mean_fitness, mean_mean_fitness + stdev_mean_fitness,
+                        color=color, alpha=0.2)
+        # plt.title(f"normal fitness vs. sigma - enemy {experiment_name[-1]}")
+        plt.xlabel("generations")
+        plt.ylabel("fitness")
+        plt.legend(loc=4)
+        # plt.ylim(-9, 80)
+        plt.savefig(f"meanfigs/neat_mean_enemy{experiment_name[-1]}", dpi=200)
+        
+    plt.show()
+
+
+if __name__ == '__main__':
+    # experiment names specified
+    experiment_names = ['neat_sigma_nhidden5_gen50_enemy', 'neat_nhidden5_gen50_enemy']
+    N_runs = 2
+
+    plot_fitness(experiment_names, N_runs, gens=50)
--- test_algorithm.py
+++ test_algorithm.py
@@ -8,7 +8,7 @@ import neat
 import pickle   
 import numpy as np
 from evoman.environment import Environment
-from player_controllers import player_controller2
+from demo_controller import player_controller
 from plot import plot_fitness
 
 # choose this for not using visuals and thus making experiments faster
@@ -20,24 +20,23 @@ pop_size = 100
 gen = 50
 n_hidden = 5
 N_runs = 10
-enemy = 6
+enemy = 5
 keep_old = 0.2
 mutation = 0.1
 
-experiment_name = f"crossover_nhidden5_gen{gen}_enemy{enemy}"
+experiment_name = f"crossover_enemy{enemy}"
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 env = Environment(experiment_name=experiment_name,
                   playermode="ai",
-                  player_controller=player_controller2(n_hidden),
+                  player_controller=player_controller(n_hidden),
                   enemies=[enemy],
                   randomini="yes")
 
 def fitness(population, i):
     pop_fitness = []
     for individual in population:
-        #reshaped = np.reshape(individual, (1,len(individual)))
         fitness = env.play(pcont=individual)[0]
         pop_fitness.append(fitness)
     
@@ -48,50 +47,56 @@ def fitness(population, i):
      
     return pop_fitness
 
-def offspring(solutions): #, old):
+def crossover(solutions): #, old):
     population, pop_fitness = solutions
     new_population = np.zeros((len(pop),len(pop[0])))
     
-    # parents_staying = int(len(population) * old)
-    # new_children = len(population) - parents_staying
-    
     # Get top 20%
+    # TODO: DO WE WANT TO USE THIS? Do we want to let only top parents breed or do we want to keep the top parents in the population
     top_parents = int(len(population) * keep_old)
     top_index = sorted(range(len(pop_fitness)), key=lambda i: pop_fitness[i])[-top_parents:]
     top_pop = [population[x] for x in top_index]
     top_fitness = [pop_fitness[x] for x in top_index]
     
     # get weights according to relative fitness
-    if (min(top_fitness) < 0):
-        positive = [x + min(top_fitness) for x in top_fitness]
+    if (min(pop_fitness) < 0):
+        positive = [x + min(pop_fitness) for x in pop_fitness]
         pop_weights = [x/sum(positive) for x in positive]
     else:
-        pop_weights = [x/sum(top_fitness) for x in top_fitness]
+        pop_weights = [x/sum(pop_fitness) for x in pop_fitness]
         
     
     # Make 100% new population with uniform crossover
-    for c in range(len(population)): #new_children):
-        # #Ugly but works
-        # c += parents_staying
-        
+    for c in range(len(population)): #new_children):        
         # Choose random parents with weights in mind
-        parents = random.choices(top_pop, weights=pop_weights, k=2)
+        # TODO: COULD ALSO CHOOSE TO TAKE MEAN OF TWO PARENTS, OR RANDOM VALUE p BETWEEN 0,1 AND GET p FROM ONE PARENT AND (1 - p) FROM THE OTHER
+        parents = random.choices(population, weights=pop_weights, k=2)
         
         # Pick every gene of the parents randomly
         parent_length = len(parents[0])
         child = np.zeros(parent_length)
         for j in range(parent_length):
-            rand = random.random()
-            print(rand)
-            if rand < mutation:
-                child[j] = random.randrange(-1, 1)
-                # print(child[j])
-            else:
-                gene = random.choice([parents[0][j], parents[1][j]])
-                child[j] = gene
+            gene = random.choice([parents[0][j], parents[1][j]])
+            child[j] = gene
+            
+        for j in range(parent_length):
+            if random.random() < mutation:
+                # TODO: Make sigma part of genes, either one sigme per child (size is n + 1) or one sigma per gene (size is n * 2)
+                sigma = 1
+                child[j] = gaussian(child[j], sigma)
+            
         new_population[c] = child
 
     return new_population
+
+def gaussian(value, sigma):
+    value += np.random.normal(0, sigma)
+    if value < -1:
+        return -1
+    elif value > 1:
+        return 1
+    else:
+        return value
 
 # number of weights for multilayer with 10 hidden neurons
 n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5
@@ -118,10 +123,11 @@ for r in range(N_runs):
     env.update_solutions(solutions)
     
     for i in range(gen):
-        pop = offspring(solutions)#, keep_old)
+        pop = crossover(solutions)#, keep_old)
         pop_fitness = fitness(pop, r)
         
         new_best = np.max(pop_fitness)
+        # TODO: REEVALUATE THE OLD BEST ?
         if new_best > best_each_gen[-1]:
             best = pop[np.argmax(pop_fitness)]
         best_each_gen.append(new_best)
@@ -135,7 +141,7 @@ for r in range(N_runs):
         
         solutions = [pop, pop_fitness]
         env.update_solutions(solutions)
-    
+    print(best)
     # Stackoverflow on how to save the winning file and open it: https://stackoverflow.com/questions/61365668/applying-saved-neat-python-genome-to-test-environment-after-training
     with open(f"{experiment_name}/winner_{r}.pkl", "wb") as f:
         pickle.dump(best, f)
